266f9d6c334607c4b01647405582e8bf034b5934
James <jbkuczma@buffalo.edu>
2017-01-20 16:18:12 -0500

    linked list

--- CTCI/ch2/node.py
+++ CTCI/ch2/node.py
@@ -30,3 +30,44 @@ class LinkedList:
 		newNode.setNext(self.head)
 		self.head = newNode
 
+	def getSize(self):
+		currentNode = self.head
+		count = 0
+		while currentNode != None:
+			count+=1
+			currentNode = currentNode.getNext()
+
+	def printList(self):
+		currentNode = self.head
+		output = ""
+		while currentNode != None:
+			output += str(currentNode.data)
+			currentNode = currentNode.getNext()
+			if currentNode != None:
+				output += ' -> '
+		print(output)
+
+	def findNode(self, node):
+		currentNode = self.head
+		while currentNode != None:
+			if currentNode.getData() == node:
+				return True
+			else:
+				currentNode = currentNode.getNext()
+		return False
+
+	def removeNode(self, node):
+		currentNode = self.head
+		previousNode = None
+		found = False
+		while not found:
+			if currentNode.getData() == node:
+				found = True
+			else:
+				currentNode = currentNode.getNext()
+		if previousNode == None:
+			self.head = currentNode.getNext()
+		else:
+			previousNode.setNext(currentNode.getNext())
+
+
