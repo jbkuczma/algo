d6237779a7239e617c64e81a301b29f2677fbc37
James <jbkuczma@buffalo.edu>
2017-02-06 09:18:11 -0500

    recursive binary search added

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -18,17 +18,45 @@ public class BinarySearch {
 		return -1;	
 	}
 
+	public static int binarySearch(int[] sortedArray, int value, int low, int high) {
+		int mid = (low + high) / 2;
+		if(low > high) {
+			return -1;
+		}
+		if(sortedArray[mid] > value) {
+			return binarySearch(sortedArray, value, low, mid-1);
+		}
+		else if(sortedArray[mid] < value) {
+			return binarySearch(sortedArray, value, mid+1, high);
+		}
+		else {
+			return mid;
+		}
+	}
+
 	public static void main(String[] args) {
 		int[] sortedArray = {0,1,2,3,4,5,6,7,8,9};
 		int value = 7;
-		System.out.println("Goal: " + value);
 		System.out.println("Iterative Binary Search");
+		System.out.println("-----------------------");
+		System.out.println("Goal: " + value);	
 		System.out.println("Index: " + binarySearch(sortedArray, value));
-
 		value = 11;
 		System.out.println("Goal: " + value);
-		System.out.println("Iterative Binary Search");
 		System.out.println("Index: " + binarySearch(sortedArray, value));
+
+		System.out.println("=======================");
+		
+		System.out.println("Recursive Binary Search");
+		System.out.println("-----------------------");
+		int low = 0;
+		int high = sortedArray.length - 1;
+		value = 4;
+		System.out.println("Goal: " + value);		
+		System.out.println("Index: " + binarySearch(sortedArray, value, low, high));
+		value = 20;
+		System.out.println("Goal: " + value);
+		System.out.println("Index: " + binarySearch(sortedArray, value, low, high));
 	}
 
 }
